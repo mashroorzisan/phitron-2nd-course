c916837e562d1b39724440f49ab36c445cd31f5b
zisan <mashroorzisan@gmail.com>
2023-11-13 09:49:44 +0600

    eof

---
+++ Module-1/end-of-file.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char a,b;
+    long long int c;
+    while(cin>>a>>b>>c){
+        cout<<"a="<<a<<endl;
+        cout<<"b="<<b<<endl;
+        cout<<"c="<<c<<endl;
+    }
+    return 0;
+}
