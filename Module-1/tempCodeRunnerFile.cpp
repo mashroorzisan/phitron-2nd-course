f266ab7fc0a145bf3fa7d1896893e924a9efc85f
zisan <mashroorzisan@gmail.com>
2023-11-13 09:45:35 +0600

    first commit- module-1-practice

---
+++ Module-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+char c;
+    cin>>c;
+    cout<<int(c)<<endl;
+    
+    char a;
+    cin>>a;
+    int ascii = a;
+    
---
+++ Module-1/typecasting.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cin>>c;
+    cout<<int(c)<<endl;
+    
+    char a;
+    cin>>a;
+    int ascii = a;
+    cout<<a<<"="<<ascii<<endl;
+    
+    long long int i;
+    cin>>i;
+    cout<<i<<endl;
+    return 0;
+}
