1d21aa1fd168f88f3577396034f855baea01cbe5
zisan <mashroorzisan@gmail.com>
2023-11-16 13:17:55 +0600

    this pointer usage

---
+++ Module-3/arrow-sign.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    public:
+        int roll;
+        int cls;
+        double gpa;
+        //constructor
+        Student(int roll, int cls, double gpa){
+           this->roll = roll;
+           this->cls  = cls;
+           this->gpa  = gpa;
+        }
+
+};
+int main(){
+    Student zisan(2008120,15,3.28);
+    cout<<zisan.roll<<endl;
+    cout<<zisan.cls<<endl;
+    cout<<zisan.gpa<<endl;
+    return 0;
+}
