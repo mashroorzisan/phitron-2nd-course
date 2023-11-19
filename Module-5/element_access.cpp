04ead60461a2280171474abc249b463f9184d070
zisan <mashroorzisan@gmail.com>
2023-11-19 12:30:23 +0600

    element access

--- Module-5/element_access.cpp
+++ Module-5/element_access.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
+    string s;
+    cin >> s;
+    cout << "frist direct - " << s[0] << endl;           // first index -directly
+    cout << "first functional - " << s.at(0) << endl;    // first index-functional
+    cout << "last direct - " << s[s.size() - 1] << endl; // last direct
+    cout << "last functional - " << s.back() << endl;    // last functional
 
     return 0;
 }
