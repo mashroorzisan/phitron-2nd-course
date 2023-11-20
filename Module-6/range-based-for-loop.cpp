e28314dba64618a8a2a97dc3590228f7b1138fed
zisan <mashroorzisan@gmail.com>
2023-11-20 20:53:55 +0600

    range based for loop

---
+++ Module-6/range-based-for-loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string string;
+    getline(cin, string);
+
+    for (char c : string)
+    {
+        cout << c << endl;
+    }
+
+    return 0;
+}
