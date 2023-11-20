7d342ca890827b55789d26c3ff437149d7928e4e
zisan <mashroorzisan@gmail.com>
2023-11-20 16:40:57 +0600

    string constructor

---
+++ Module-6/string-constructor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // 1.string a = "hello world";
+    // 2.string a("hello world");
+    // 3.string a("hello world", 3);
+    // 4. string a = "hello world";
+    // string b(a, 3);
+    // 5. string a(20, '`');
+    cout
+        << a;
+
+    return 0;
+}
